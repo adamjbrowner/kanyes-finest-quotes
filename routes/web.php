503d181b12763c99eed470e9f5212409afca46ad
Adam Browner-Denning <adamjbrowner@gmail.com>
2024-04-14 12:06:35 +0100

    Added very simple token check for api route

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\KanyeQuoteController;
+use App\Http\Middleware\CheckToken;
 
 Route::get('/', function () {
     return view('app');
 });
 
-Route::group(['middleware' => ['throttle:60,1']], function () {
+Route::group(['middleware' => [CheckToken::class, 'throttle:60,1']], function () {
     Route::get('/kanye-quotes', KanyeQuoteController::class);
 });
