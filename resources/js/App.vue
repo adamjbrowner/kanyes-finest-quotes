fc63a8ce278a7eab7e5162f24a439ebfc8753df7
Adam Browner-Denning <adamjbrowner@gmail.com>
2024-04-14 11:50:51 +0100

    Added quotes to vue component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,36 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const kanye_emoji = '/images/kanye-west-emoji.png';
+const quotes = ref([]);
+const buttonText = ref('Get Quotes');
+const possibleButtonTexts = ['More Kanye', 'More!', 'Hit Me', 'I have not seen enough quotes'];
+
+const getQuotes = async () => {
+    const response = await axios.get('/kanye-quotes');
+    quotes.value = response.data;
+    buttonText.value = possibleButtonTexts[Math.floor(Math.random() * possibleButtonTexts.length)];
+};
+
+
+</script>
+
 <template>
      <div>
         <div style="display: flex">
             <h1>
-                Kanye Quotes
+                Kanye's Finest Oratory Moments
             </h1>
             <img :src="kanye_emoji" alt="Kanye West" height="100px" />
         </div>
+        <button @click="getQuotes">
+            {{ buttonText }}
+        </button>
+        <ul>
+            <li v-for="quote in quotes" :key="quote">
+                {{ quote }}
+            </li>
+        </ul>
     </div>
 </template>
