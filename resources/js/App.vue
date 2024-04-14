d52e762844834d2cfe8f26f87981558f571b7300
Adam Browner-Denning <adamjbrowner@gmail.com>
2024-04-14 11:13:18 +0100

    First commit, added vue component and simple route

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+     <div>
+        <div style="display: flex">
+            <h1>
+                Kanye Quotes
+            </h1>
+            <img :src="kanye_emoji" alt="Kanye West" height="100px" />
+        </div>
+    </div>
+</template>
